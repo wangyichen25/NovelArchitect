718cd7f27783a6ccf1538c8912c1dbdb14561e1e
Wang <wang.yichen@r5519856.mayo.edu>
2025-12-10 00:18:32 -0500

    Your commit message here

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -6,8 +6,8 @@ export const createClient = async () => {
     const cookieStore = await cookies()
 
     return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
+        process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://slapnnvkgclgfwffhacc.supabase.co',
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_5P4LEe_txEHnYW9DL7GKkg_fYfr5jgn',
         {
             cookies: {
                 getAll() {
--- src/middleware.ts
+++ src/middleware.ts
@@ -10,8 +10,8 @@ export async function updateSession(request: NextRequest) {
     })
 
     const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
+        process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://slapnnvkgclgfwffhacc.supabase.co',
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_5P4LEe_txEHnYW9DL7GKkg_fYfr5jgn',
         {
             cookies: {
                 getAll() {
